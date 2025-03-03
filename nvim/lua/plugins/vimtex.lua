4afe69de69d167db636be25fdf8484783619eeeb
weisshymmnos <weisshymmnos@icloud.com>
2025-03-03 20:08:33 +0800

    Solved vim issue

--- nvim/lua/plugins/vimtex.lua
+++ nvim/lua/plugins/vimtex.lua
@@ -2,8 +2,15 @@ return {
   "lervag/vimtex",
   config = function()
     -- 配置 VimTeX
-    vim.g.vimtex_view_method = 'zathura'
-    vim.g.vimtex_compiler_method = 'xelatex'
-    vim.g.vimtex_fold_enabled = 1
+        vim.g.vimtex_view_method = 'zathura'
+	vim.g.vimtex_compiler_method = 'latexmk'  -- 使用 latexmk 作为编译工具
+	vim.g.vimtex_compiler_latexmk = {
+	  build_dir = './build',  -- 设置构建目录
+	  executable = 'xelatex',  -- 设置具体的编译器为 xelatex
+	  options = {
+	    '-interaction=nonstopmode',  -- 设置非交互模式
+	    '-synctex=1',  -- 启用 Synctex
+	  },
+	}    vim.g.vimtex_fold_enabled = 1
   end,
 }
